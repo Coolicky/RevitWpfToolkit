da410b3e7ad4d39175bdd4c54cb691114f305a74
coolicky <piotr.kulicki@gmail.com>
2023-02-23 21:34:32 +0000

    updated workflow, added release publish

--- build/Build.Properties.cs
+++ build/Build.Properties.cs
@@ -1,16 +1,12 @@
 using Nuke.Common;
-using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tools.GitVersion;
 
 public partial class Build
 {
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
-
-    [GitVersion] readonly GitVersion GitVersion;
-    [GitRepository] readonly GitRepository GitRepository;
+    
     [Solution] readonly Solution Solution;
 
     const string ArtifactsFolder = "output";
